0a24bce117c4b0f7fa34bce3796ce265aed6cd40
palalle <palalle1024@gmail.com>
2025-05-27 11:52:46 -0500

    rabbit

--- common.sh
+++ common.sh
@@ -95,7 +95,7 @@ VALIDATE(){
 
 print_time(){
     END_TIME=$(date +%s)
-    TOTAL_TIME=$( ( $END_TIME - $START_TIME ) )
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
     echo "Script executed successfully, $Y Time taken: $TOTAL_TIME seconds $N"
 }
 
