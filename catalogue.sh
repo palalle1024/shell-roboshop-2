baeb639de1752e151ca772e91746b9edbe40e4db
palalle <palalle1024@gmail.com>
2025-05-29 07:17:04 -0500

    hello world

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 source ./common.sh
 app_name=catalogue
 
@@ -8,21 +10,17 @@ app_setup
 nodejs_setup
 systemd_setup
 
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB Client"
 
-
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
-VALIDATE $? "copying mongoDB repo"
-
-dnf install mongodb-mongosh -y &>>LOG_FILE
-VALIDATE $? "Installing Mongodb client"
-
-
-STATUS=$(mongosh --host mongodb.palalle.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.palalle.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
 
+print_time
